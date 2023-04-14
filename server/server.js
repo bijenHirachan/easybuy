57e28a0116702767078cb2b5fa9906be692bcb67
Bijen Hirachan <bijen.hirachan@gmail.com>
2023-04-14 22:44:39 +0200

    Listening after connecting mongodb

--- server/server.js
+++ server/server.js
@@ -1,20 +1,10 @@
-import app from "./app.js";
 import connectDB from "./database.js";
 import cloudinary from "cloudinary";
+
+connectDB();
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
-
-const PORT = process.env.PORT || 4000;
-
-const startApp = async () => {
-  await connectDB();
-  app.listen(PORT, () => {
-    console.log(`Server listening on PORT: ${PORT}`);
-  });
-};
-
-startApp();
